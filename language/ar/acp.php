8118b3de2fc55a561994a1dd95d7f8f0f7bdc544
Basil Taha Alhitary <alhitary@alhitary.net>
2014-12-31 13:13:42 +0300

    Create acp.php

---
+++ language/ar/acp.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+* @package phpBB Extension - marttiphpbb posting template
+* @copyright (c) 2014 marttiphpbb <info@martti.be>
+* @license http://opensource.org/licenses/MIT
+*
+* Translated By : Basil Taha Alhitary - www.alhitary.net
+*/
+
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+
+
+
+$lang = array_merge($lang, array(
+	'ACP_POSTINGTEMPLATE'			=> 'قالب المشاركة ',
+	'ACP_POSTINGTEMPLATE_EXPLAIN'	=> 'عند إضافة موضوع جديد في هذا المنتدى , سيتم إضافة هذا القالب تلقائياً إلى مربع النص. اتركه فارغاً لتعطيل هذا الخيار.', 
+));
