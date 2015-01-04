ba7f388c75a8177213c92370340b812cd6f3fb0d
martti <info@martti.be>
2015-01-04 09:59:03 +0100

    multibyte

--- event/listener.php
+++ event/listener.php
@@ -60,7 +60,7 @@ class listener implements EventSubscriberInterface
 		$forum_data = $event['forum_data'];
 		$forum_id = $forum_data['forum_id'];
 
-		$postingtemplate = $this->request->variable('forum_postingtemplate', '');
+		$postingtemplate = $this->request->variable('forum_postingtemplate', '', true);
 
 		if ($postingtemplate)
 		{
