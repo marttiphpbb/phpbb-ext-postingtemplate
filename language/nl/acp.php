77d75ca10fa0e5ea7f5b10eae0fd1b318be61f72
martti <info@martti.be>
2015-01-18 00:24:32 +0100

    Dutch Translation
    This reverts commit 7da1d8c0536a8c39b5daf37758963bf0f224a0d8.

---
+++ language/nl/acp.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+* phpBB Extension - marttiphpbb 
+* @copyright (c) 2014 marttiphpbb <info@martti.be>
+* @license http://opensource.org/licenses/MIT
+* [Dutch] translated by Dutch Translators (https://github.com/dutch-translators)
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+
+$lang = array_merge($lang, array(
+	'ACP_POSTINGTEMPLATE'			=> 'Posting template',
+	'ACP_POSTINGTEMPLATE_EXPLAIN'	=> 'Bij het starten van een onderwerp, komt er in het tekstveld een vooraf ingesteld posting template te staan. Laat dit vak leeg als je deze optie niet wilt gebruiken.',
+));
